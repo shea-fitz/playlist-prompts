af4b261d9330084b19a458f60a2da4a3212660c1
shea-fitz <sheafitz10@gmail.com>
2021-04-16 15:58:04 -0400

    link channel

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import './tachyons.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 
 function App() {
+
+  const [prompts, setPrompts] = useState(null);
+
+
+  useEffect(() => {
+    axios.get('https://api.are.na/v2/channels/playlist-prompts').then(res => {
+      setPrompts(res.data.contents);
+    }).catch(err => console.log(err));
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>playlist prompts</h1>
+
+      <div>
+        {prompts && prompts.map(el => {
+          return (<p key={el.key}>
+            {el.content}
+            </p>)
+        })}
+      </div>
     </div>
   );
 }
