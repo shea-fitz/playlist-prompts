d87ddcf0a805aecd8cb49e95619361e792b73a88
shea-fitz <sheafitz10@gmail.com>
2022-08-14 15:11:56 -0400

    header, shuffle on load

--- src/App.js
+++ src/App.js
@@ -8,11 +8,28 @@ import Paginate from './components/Paginate';
 
 
 function App() {
-  const postsPerPage = 25;
+  const postsPerPage = 100;
   const [prompts, setPrompts] = useState(null);
   const [total, setTotal] = useState(1);
   const [page, setPage] = useState(1);
 
+  function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
 
   useEffect(() => {
     axios.get('https://api.are.na/v2/channels/playlist-prompts', 
@@ -20,10 +37,12 @@ function App() {
       per: postsPerPage,
       page: page}
     }).then(res => {
-      setPrompts(res.data.contents);
+      setPrompts(shuffle(res.data.contents));
       setTotal(Math.ceil(res.data.length / postsPerPage));
     }).catch(err => console.log(err));
   }, [page]);
+
+  
 
   return (
     <div className="wrapper">
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -13,12 +13,39 @@ const Intro = () => {
 
 return(
     <>
-        <div className="intro" onClick={handleClick}>about</div>
+
+    <div className="header">
+        Playlist Prompts
+    </div>
+
+    <div className="about" onClick={handleClick}>about</div>
+
+
+
+
+
         <Fade when={visibility}>
         <div className={visibility ? "intro-modal" : "hide"}>
-            <div>
-                Hello welcome to playlist prompts
-            </div>
+            <p>
+            Playlist Prompts was started by <a href="https://www.are.na/shea" target="_blank">Shea Fitzpatrick</a> in 2020 and so far has taken form as:
+            </p>
+               
+            <ul className="list">
+                <li><span className="year">[ ongoing ]</span>An ambient collaboration between <a href="https://www.are.na/shea/playlist-prompts" target="_blank">internet strangers</a></li>
+
+                <li><span className="year">[ 2020 ]</span>A participatory <a href="https://tinyletter.com/playlistprompts" target="_blank">newsletter</a></li>
+
+                <li><span className="year">[ 2021 ]</span>A poem co-written with Mad Hsia and published in the <a href="https://store.are.na/products/are-na-annual-2021?variant=38246218563735" target="_blank">Are.na Annual</a></li>
+                <li><span className="year">[ 2022 ]</span>This website</li>
+            </ul>
+
+            <p>For now, submissions and responses are welcomed on <a href="https://www.are.na/shea/playlist-prompts" target="_blank">Are.na</a>, where it began.</p>
+
+            <p className="credits">This site was designed and developed by Shea with support from <a href="https://www.jbell.life/" target="_blank">Julia Bell.</a>
+            
+            </p>
+
+
         </div>
         </Fade>
       
